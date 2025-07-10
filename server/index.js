fe71dce101c2e97afe63b02352499a7ccc09574e
maki-hashimotooo <mhashimoto1997@gmail.com>
2025-07-10 18:30:43 +0900

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const distPath = path.join(__dirname, '..', 'client', 'dist');
+app.use(express.static(distPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
